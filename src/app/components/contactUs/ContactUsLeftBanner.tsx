99d36f7e7870453e0eb8e238bba81578200f613b
Saish <saish.kothare@gmail.com>
2024-07-25 09:40:40 +0800

    Navbar tab hover update

--- src/app/components/contactUs/ContactUsLeftBanner.tsx
+++ src/app/components/contactUs/ContactUsLeftBanner.tsx
@@ -10,7 +10,7 @@ const ContactUsLeftBanner = () => {
       <Typography variant={TypographyVariant.H2} className='mt-0'>
         Contact Us
       </Typography>
-      <Typography variant={TypographyVariant.Body1}>
+      <Typography variant={TypographyVariant.Body1} color='var(--GhostWhite)'>
         We will try our best to get back to you as soon as possible
       </Typography>
     </div>
